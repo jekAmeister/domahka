8313b933392af09de0c7a200614e7073d95f893d
Евгений Соловьёв <jeka777925@gmail.com>
2024-06-23 20:32:01 +0300

    fixes

---
+++ src/widget.py
@@ -0,0 +1,25 @@
+from src.masks import get_mask_account, get_mask_card_number
+
+
+def account_card_disguise(string: str) -> str:
+    """Функция которая маскирует номер карты и счета"""
+
+    if "Счет" in string:
+
+        return get_mask_account(string)
+    else:
+        return get_mask_card_number(string)
+
+
+print(account_card_disguise("Visa Platinum 8990922113665229"))
+
+
+def get_data(data_string: str) -> str:
+    """Функция преобразования даты"""
+
+    data_conversion = (data_string[-18:-16], data_string[-22:-18], data_string[:-22])
+
+    return "".join(data_conversion)
+
+
+print(get_data("2018-07-11T02:26:18.671407"))
