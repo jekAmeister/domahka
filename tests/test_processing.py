670b388497833373487e3743b577510d5c1898ef
Евгений Соловьёв <jeka777925@gmail.com>
2024-11-24 19:59:27 +0300

    init  commit test_processing.py

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,4 +1,4 @@
-from src.processing import filter_by_state
+from src.processing import filter_by_state,sort_by_date
 import pytest
 
 def test_filter_by_state(test_list_by_state,test_correct_answer) -> None:
@@ -17,19 +17,16 @@ def test(arg, expected,fixture):
 
 
 
+def test_sort_by_date(test_list_by_state,test_execution)->None:
+    """Тест для проверки функции которая сортирует список словарей по параметру date, если даты одинаковые"""
+    assert sort_by_date(test_list_by_state) == test_execution
 
 
+@pytest.fixture
+def fixture():
+   return ["shared fixture"]
 
-
-
-
-
-
-
-# def test_sort_by_date(by_date: list, reverse: bool = True) -> None:
-#     assert sort_by_date([
-#             {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
-#             {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
-#             {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
-#             {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
-#         ]) == by_date
+@pytest.mark.parametrize("arg,expected", [('by_state_stan','"state" not found'),('test_by_state_stan', '"state" not found')])
+def test(arg, expected,fixture):
+    """Тест на срабатывание функции при введении данных с ошибкой"""
+    assert filter_by_state(fixture,arg) == expected
