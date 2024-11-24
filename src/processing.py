83a0f449dac524868d9f18a98aa919e6de7970ce
Евгений Соловьёв <jeka777925@gmail.com>
2024-11-24 19:58:56 +0300

    init  commit processing.py

--- src/processing.py
+++ src/processing.py
@@ -1,16 +1,16 @@
-def filter_by_state(inform_state: list, state_clue: object = "EXECUTED") -> list:
+def filter_by_state(list_by_state: list[{}], state: object ="EXECUTED") -> list[{}]:
     """Функция которая принимает на вход список словарей и значение для ключа
     state(опциональный параметр со значением по умолчанию EXECUTED)
     и возвращает новый список, содержащий только те словари, у которых ключ state
     содержит переданное в функцию значение."""
 
-    list_state = []
 
-    for key in inform_state:
-        if key.get("state") == state_clue:
-            list_state.append(key)
-    return list_state
 
+    for i in list_by_state:
+       if "state" in i:
+            return [item for item in list_by_state if item.get("state") == state]
+       if "state" not in i:
+           return '"state" not found'
 
 print(
     filter_by_state(
@@ -23,11 +23,23 @@ print(
     )
 )
 
+def sort_by_date(list_of_dict: list, reverse=True):
+    """Функция возвращает отсртированный список по дате"""
+    for i in list_of_dict:
+        if "date" in i:
+            return sorted(list_of_dict, key=lambda x: x.get("date", ""), reverse=reverse)
+        else:
+            return '"date" not found'
 
-def sort_by_date(dictionaries: list, reverse: bool = True) -> list:
-    """Функция функцию, которая принимает на вход список словарей и возвращает новый список,
-    в котором исходные словари отсортированы по убыванию даты (ключ date)"""
-    return sorted(dictionaries, key=lambda dictionary: dictionary["date"], reverse=True)
+
+
+
+# def sort_by_date(dictionaries: list[dict[str]]) -> list[dict[str]] :
+#     """Функция функцию, которая принимает на вход список словарей и возвращает новый список,
+#     в котором исходные словари отсортированыпо убыванию даты (ключ date)"""
+#     dictionaries_reverse = []
+#     dictionaries_reverse = sorted(dictionaries, key=lambda dictionary: dictionary["date"], reverse=True)
+#     return dictionaries_reverse
 
 
 print(
@@ -43,6 +55,7 @@ print(
 
 
 if __name__ == '__processing__':
+
     assert filter_by_state( [
             {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
             {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
@@ -60,3 +73,5 @@ if __name__ == '__processing__':
                 {'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'},
                 {'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
                 {'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'}])
+
+
