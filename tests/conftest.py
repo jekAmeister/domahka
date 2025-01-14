4ec873c2b7c611d439ea2a1010993fba00f36bcf
Евгений Соловьёв <jeka777925@gmail.com>
2025-01-14 21:09:55 +0300

    init  commit conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+from random import choices
+
 import pytest
 
 @pytest.fixture
@@ -80,3 +82,112 @@ def test_list_adaptations():
 def test_list_answer():
     return 'test_card_correct','test_result_account'
 
+@pytest.fixture
+def test_transactions():
+    return [
+    {
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702",
+    },
+    {
+        "id": 142264268,
+        "state": "EXECUTED",
+        "date": "2019-04-04T23:20:05.206878",
+        "operationAmount": {"amount": "79114.93", "currency": {"name": "EUR", "code": "EUR"}},
+        "description": "Перевод со счета на счет",
+        "from": "Счет 19708645243227258542",
+        "to": "Счет 75651667383060284188",
+    },
+    {},
+    {
+        "id": 142264269,
+        "state": "EXECUTED",
+        "date": "2019-04-04T23:20:05.206878",
+        "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
+        "description": "Перевод со счета на счет",
+        "from": "Счет 19708645243227258542",
+        "to": "Счет 75651667383060284188",
+    },
+]
+
+@pytest.fixture
+def test_transactions_wrong():
+    return [
+    {
+        "id": 939719570,
+        "state": "EXECUTED",
+        "date": "2018-06-30T02:08:58.425572",
+        "operationAmount": {"amount": "9824.07", "currency": {"name": "RUB", "code": "RUB"}},
+        "description": "Перевод организации",
+        "from": "Счет 75106830613657916952",
+        "to": "Счет 11776614605963066702",
+    },
+    {
+        "id": 142264268,
+        "state": "EXECUTED",
+        "date": "2019-04-04T23:20:05.206878",
+        "operationAmount": {"amount": "79114.93", "currency": {"name": "EUR", "code": "EUR"}},
+        "description": "Перевод со счета на счет",
+        "from": "Счет 19708645243227258542",
+        "to": "Счет 75651667383060284188",
+    },
+    {},
+    {
+        "id": 142264269,
+        "state": "EXECUTED",
+        "date": "2019-04-04T23:20:05.206878",
+        "operationAmount": {"amount": "79114.93", "currency": {"name": "RUB", "code": "RUB"}},
+        "description": "Перевод со счета на счет",
+        "from": "Счет 19708645243227258542",
+        "to": "Счет 75651667383060284188",
+    },
+]
+
+@pytest.fixture
+def test_result_filter_by_currency():
+    return [{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572','operationAmount': {'amount': '9824.07', 'currency': {'name': 'USD', 'code': 'USD'}},'description': 'Перевод организации', 'from': 'Счет 75106830613657916952', 'to': 'Счет 11776614605963066702'},
+            {'id': 142264269, 'state': 'EXECUTED', 'date': '2019-04-04T23:20:05.206878','operationAmount': {'amount': '79114.93', 'currency': {'name': 'USD', 'code': 'USD'}},'description': 'Перевод со счета на счет', 'from': 'Счет 19708645243227258542', 'to': 'Счет 75651667383060284188'}]
+
+@pytest.fixture
+def test_filter_by_currency_incorrect_data():
+    return []
+
+
+@pytest.fixture
+def test_different_input_data():
+    return []
+
+
+@pytest.fixture
+def test_result_transaction_descriptions():
+    return ['Перевод организации',
+           'Перевод со счета на счет',
+           'Перевод со счета на счет']
+
+
+@pytest.fixture
+def test_result_different_input_data():
+    return 'Список описания транзакций пуст'
+
+
+@pytest.fixture
+def test_expected_numbers():
+    return ["0000 0000 0000 0001",
+            "0000 0000 0000 0002",
+            "0000 0000 0000 0003",
+            "0000 0000 0000 0004",
+            "0000 0000 0000 0005",
+            ]
+
+@pytest.fixture
+def test_expected_numbers_incorrect_data():
+    return []
+
+@pytest.fixture
+def test_expected_numbers_input_data():
+    return 'Список пуст'
